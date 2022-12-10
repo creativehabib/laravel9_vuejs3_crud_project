c0e6d47344f3dca74ffcb856a3529cd4269ceee1
creativehabib <shamolrahaman@gmail.com>
2022-12-10 14:09:37 +0600

    first and comp;eted project

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ApplicationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/admin/dashboard', function () {
+//     return view('dashboard');
+// });
+
+Route::get('/api/users', [UserController::class, 'index']);
+Route::post('/api/users', [UserController::class, 'store']);
+Route::patch('/api/users/{user}/change-role', [UserController::class, 'changeRole']);
+Route::put('/api/users/{user}', [UserController::class, 'update']);
+Route::delete('/api/users/{user}', [UserController::class, 'destory']);
+
+Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
